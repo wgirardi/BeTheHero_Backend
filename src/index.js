6d43f887670dede27c2860d442f88f13c904a955
Weber Girardi <webergirardi@iMac-Weber-Girardi.local>
2020-03-30 21:45:15 -0400

    Backend para Heroku

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require ('express');
+const cors = require('cors');
+const { errors } = require('celebrate');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use(errors());
+
+app.listen(process.env.PORT || 3333);
+
+// no Heroku alterar a opcao da porta 
